8e6b024fe2e5b24025ef0015e93cb6763517617e
TreeOck <aditya.nair@students.iiit.ac.in>
2022-12-13 18:00:12 +0530

    Some issue with Boss Music, fixed that

--- HHH/Assets/Scripts/BossBattleTrigger.cs
+++ HHH/Assets/Scripts/BossBattleTrigger.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class BossBattleTrigger : MonoBehaviour
 {
     public GameObject cardinalEnemy;
+    public AudioSource bossMusic;
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player")) {
             Instantiate(cardinalEnemy, transform.position, transform.rotation);
+            bossMusic.Play();
         }
     }
 }
--- HHH/Assets/Scripts/Enemy/CardinalAttributes.cs
+++ HHH/Assets/Scripts/Enemy/CardinalAttributes.cs
@@ -9,6 +9,7 @@ public class CardinalAttributes : MonoBehaviour
     public float maxCardinalHp = 20;
 
     private Slider cardinalHpDisplaySlider;
+    public AudioSource bossMusic;
 
     private void Start() {
         cardinalHpDisplaySlider = transform.Find("Cardinal HP Canvas").transform.Find("Cardinal HP Slider").GetComponent<Slider>();
@@ -19,6 +20,7 @@ public class CardinalAttributes : MonoBehaviour
         cardinalHp -= damageValue;
         if(cardinalHp <= 0) {
             Destroy(gameObject);
+            bossMusic.Stop();
         }
         cardinalHpDisplaySlider.value = cardinalHp/maxCardinalHp;
     }
